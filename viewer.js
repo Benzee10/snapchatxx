9de5cb834e57ac38879c0aa8fee16dde500e1e1b
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-06-17 15:16:18 +0100

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabase = createClient(
-  "https://your-project-id.supabase.co",
-  "your-anon-key"
+  "https://vcgexvujgvfwbaktnwry.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZ2V4dnVqZ3Zmd2Jha3Rud3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNjUzNDgsImV4cCI6MjA2NTc0MTM0OH0.6DvFHYliTSOloejLDWysgpjvS0f5YAm61q1Zq-OMU8k"
 );
 
 let models = [];
