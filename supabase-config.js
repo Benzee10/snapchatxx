2d625ddd08d069197d4477edc12ff196e88eaa5f
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-09-09 09:46:07 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-export const supabase = createClient(
-  'https://vcgexvujgvfwbaktnwry.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZ2V4dnVqZ3Zmd2Jha3Rud3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNjUzNDgsImV4cCI6MjA2NTc0MTM0OH0.6DvFHYliTSOloejLDWysgpjvS0f5YAm61q1Zq-OMU8k'
-);
